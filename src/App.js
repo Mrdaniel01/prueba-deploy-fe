55e769689c9697766f577d9c2446ede482e1dacb
Juan Rios <mrdaniel19996@gmail.com>
2020-09-16 20:59:27 -0500

    [ADD]Prueba para deploy

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+import Nav from './components/Nav'
+import Product from './pages/Product';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Product />
+      home
+    </div>
+  );
+}
+
+export default App;
